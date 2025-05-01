799061a74647b4ddb0f30354b60181c307811ebd
yashvi03 <yashviagrawal03@gmail.com>
2025-05-01 16:30:37 +0530

    updated docker compose

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,10 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgres://u2l7qdtl26uu9r:pb3db3b483d4e568a8f7f8ffb68cd665bc8b1166170e2392d11bee5a8ec8734c0@ec2-3-232-82-232.compute-1.amazonaws.com:5432/d3pii0o3i8o5co'
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     SECRET_KEY = '5b7c3a4f08d19e6e71fa0b9a2d389bhe'  
