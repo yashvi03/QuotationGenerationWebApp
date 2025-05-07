be7c4a1dfacad2568d02993bb4cd7a725132f111
yashvi03 <yashviagrawal03@gmail.com>
2025-05-07 10:29:34 +0530

    fixed url

--- webAppFrontend/src/services/axiosInstance.jsx
+++ webAppFrontend/src/services/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/",
+  baseURL: 'https://puranmalsons-quotation-webapp-0b4c571a2cc2.herokuapp.com/',
   headers: {
     "Content-Type": "application/json",
   },
